0bfb60949add0653d0be1e2b50c8957f4e39cd18
LukaStjc <luka2001stajic@gmail.com>
2023-11-13 21:41:40 +0100

    Some changes in App.js file but not working correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 //import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListCompaniesComponent from './components/ListCompaniesComponent';
+import CreateCompanyComponent from './components/CreateCompanyComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListCompaniesComponent/>
+    <div className = "container">
+
+    <Router>
+      <Routes>
+        <Route path="/api/companies" component={ListCompaniesComponent} exact/>
+        <Route path="/api/companies/create" component={CreateCompanyComponent} exact/>
+      </Routes>
+    </Router>
+
+    <ListCompaniesComponent/>
     </div>
+    
+
   );
 }
 
