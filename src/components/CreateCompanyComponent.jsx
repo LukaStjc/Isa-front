3e006303d6212a177b7b396b0a9c7578a118838e
LukaStjc <luka2001stajic@gmail.com>
2023-11-15 16:16:15 +0100

    Tried to import and implement time picker, didn't finish

--- src/components/CreateCompanyComponent.jsx
+++ src/components/CreateCompanyComponent.jsx
@@ -1,7 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
 import CompanyService from '../services/CompanyService';
 
+
 class CreateCompanyComponent extends Component {
+    
+
     constructor(props){
         super(props)
 
@@ -11,19 +18,20 @@ class CreateCompanyComponent extends Component {
             country: '',
             city: '',
             streetName: '',
-            streetNumber: 0,
-            // openingTime: '',
+            streetNumber: '',
+            openingTime: '',
             // closingTime: '',
         }
 
-        this.changeNameHandler = this.changeNameHandler.bind(this);
-        this.changeDescriptionHandler = this.changeDescriptionHandler.bind(this);
-        this.changeCountryHandler = this.changeCountryHandler.bind(this);
-        this.changeCitynHandler = this.changeCitynHandler.bind(this);
-        this.changeStreetNameHandler = this.changeStreetNameHandler.bind(this);
-        this.changeStreetNumberHandler = this.changeStreetNumberHandler.bind(this);
+        this.changeNameHandler = this.changeNameHandler.bind(this)
+        this.changeDescriptionHandler = this.changeDescriptionHandler.bind(this)
+        this.changeCountryHandler = this.changeCountryHandler.bind(this)
+        this.changeCitynHandler = this.changeCitynHandler.bind(this)
+        this.changeStreetNameHandler = this.changeStreetNameHandler.bind(this)
+        this.changeStreetNumberHandler = this.changeStreetNumberHandler.bind(this)
+        this.changeOpeningTimeHanler = this.changeOpeningTimeHanler.bind(this)
 
-        this.saveCompany = this.saveCompany.bind(this);
+        this.saveCompany = this.saveCompany.bind(this)
     }
 
     changeNameHandler=(event) =>{
@@ -50,6 +58,10 @@ class CreateCompanyComponent extends Component {
         this.setState({streetNumber: event.target.value})
     }
 
+    changeOpeningTimeHanler=(event) =>{
+        this.setState({openingTime: event.target.value})
+    }
+
     saveCompany= async(e) =>{
         e.preventDefault();
         let company = {name: this.state.name, description: this.state.description, country: this.state.country, 
@@ -70,12 +82,14 @@ class CreateCompanyComponent extends Component {
         }
         
         
-        //window.location.reload(); // jer nece da mi ucita komponent koji je na /api/companies putanji
+        window.location.reload(); // jer nece da mi ucita komponent koji je na /api/companies putanji
     }
 
     render() {
+        //const [selectedDate, handleTimeChange] = useState(new Date())
+
         return (
-            <div>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
                 <div className='container'>
                     <div className='row'>
                         <div className='card col-md-6 offset-md-3 offset-md-3'>
@@ -85,39 +99,41 @@ class CreateCompanyComponent extends Component {
                                     <div className='form-group'>
                                         <label>Company name: </label>
                                         <input placeholder='Name' name='name' className='form-control'
-                                               value={this.state.name} onChange={this.changeNameHandler}/>                                      
+                                                value={this.state.name} onChange={this.changeNameHandler}/>                                      
                                     </div>
 
                                     <div>
                                         <label>Company description: </label>
                                         <input placeholder='Description' name='description' className='form-control'
-                                               value={this.state.description} onChange={this.changeDescriptionHandler}/>
+                                                value={this.state.description} onChange={this.changeDescriptionHandler}/>
                                     </div>
 
                                     <div>
                                         <label>Country the company is in: </label>
                                         <input placeholder='Country' name='country' className='form-control'
-                                               value={this.state.country} onChange={this.changeCountryHandler}/>
+                                                value={this.state.country} onChange={this.changeCountryHandler}/>
                                     </div>
 
                                     <div>
                                         <label>City the company is in: </label>
                                         <input placeholder='City' name='city' className='form-control'
-                                               value={this.state.city} onChange={this.changeCitynHandler}/>
+                                                value={this.state.city} onChange={this.changeCitynHandler}/>
                                     </div>
 
                                     <div>
                                         <label>Name of the street the company located on: </label>
                                         <input placeholder='Name' name='description' className='form-control'
-                                               value={this.state.streetName} onChange={this.changeStreetNameHandler}/>
+                                                value={this.state.streetName} onChange={this.changeStreetNameHandler}/>
                                     </div>
 
                                     <div>
                                         <label>Street number : </label>
                                         <input placeholder='Number' name='description' className='form-control'
-                                               value={this.state.streetNumber} onChange={this.changeStreetNumberHandler}/>
+                                                value={this.state.streetNumber} onChange={this.changeStreetNumberHandler}/>
                                     </div>
 
+                                    <TimePicker value={this.state.openingTime} onChange={this.changeOpeningTimeHanler}></TimePicker>
+                                        
                                     <button className='btn btn-success' onClick={this.saveCompany}>Save</button>
                                     {/*<button className='btn btn-success' onClick={this.changeCitynHandler.bind(this)} style={{marginLeft: "10px"}}>
                                         Cancel
@@ -127,7 +143,7 @@ class CreateCompanyComponent extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </LocalizationProvider>
         );
     }
 }
