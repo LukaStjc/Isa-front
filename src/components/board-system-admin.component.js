5ae605be1566db5fe8cc31a4c238d56d12efa4cc
Vasilije <zekovic.in4.2020@uns.ac.rs>
2024-06-11 12:55:30 +0200

    The basic order delivery simulator is implemented

--- src/components/board-system-admin.component.js
+++ src/components/board-system-admin.component.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
-
 import UserService from "../services/user.service";
 import EventBus from "../common/EventBus";
+import { Client } from '@stomp/stompjs';
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+
+const customIcon = new Icon({
+  iconUrl: require("../location-pin.png"),
+  iconSize: [38, 38]
+})
 
 export default class BoardSystemAdmin extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      content: ""
+      content: "",
+      vehicleLocations: []
     };
+
+    // Configure WebSocket connection
+    this.client = new Client({
+      brokerURL: "ws://localhost:8082/ws/websocket",  // Update your WebSocket URL here
+      debug: function (str) {
+        console.log('STOMP: ' + str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: () => {
+        this.onConnected();
+      },
+      onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+      },
+    });
   }
 
   componentDidMount() {
@@ -21,12 +48,8 @@ export default class BoardSystemAdmin extends Component {
       },
       error => {
         this.setState({
-          content:
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
+          content: (error.response && error.response.data && error.response.data.message) ||
+            error.message || error.toString()
         });
 
         if (error.response && error.response.status === 401) {
@@ -34,13 +57,48 @@ export default class BoardSystemAdmin extends Component {
         }
       }
     );
+
+    this.client.activate();  // Activating the STOMP Client
   }
 
+  componentWillUnmount() {
+    this.client.deactivate();  // Deactivating the STOMP Client
+  }
+
+  onConnected = () => {
+    // Subscribe to the topic and handle incoming messages
+    this.client.subscribe('/topic/vehicleLocation', (message) => {
+      if (message.body) {
+        const newLocation = JSON.parse(message.body)
+        const locationWithTimestamp = {
+          ...newLocation, 
+          timeStamp: new Date().toLocaleTimeString()
+        };
+        this.setState(prevState => ({
+          vehicleLocations: [...prevState.vehicleLocations, locationWithTimestamp]
+        }))
+      }
+    });
+  };
+
   render() {
+    const { content, vehicleLocations } = this.state;
     return (
       <div className="container">
         <header className="jumbotron">
-          <h3>{this.state.content}</h3>
+          <h3>{content}</h3>
+          {vehicleLocations.length > 0 && (
+            <MapContainer center={[vehicleLocations[vehicleLocations.length - 1].latitude, vehicleLocations[vehicleLocations.length - 1].longitude]} zoom={13} style={{ height: 400, width: "100%" }}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              {vehicleLocations.map((location, index) => (
+                <Marker key={index} position={[location.latitude, location.longitude]} icon={customIcon}>
+                  <Popup>Vehicle was here at {location.timeStamp}</Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          )}
         </header>
       </div>
     );
